302ace2f852b1bd3c97b46a25a1c3f8ab9083927
Jcub-C <jwfchong2012@yahoo.ca>
2024-09-13 10:08:35 -0700

    starting up new
    mostly did the nav

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
-    $('.experiment-image-2').fadeToggle("slow");
-    // Alternative animation for example
-    // slideToggle("fast");
-  });
+document.getElementById("menu-toggle").addEventListener("change", function () {
+  const menu = document.querySelector(".menu");
+  if (this.checked) {
+    menu.style.display = "flex"; // Show the menu
+  } else {
+    menu.style.display = "none"; // Hide the menu
+  }
 });
