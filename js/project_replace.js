f6a1da4ad68f791aebf9cc959ba90736298570f1
Jcub-C <jwfchong2012@yahoo.ca>
2025-04-16 13:09:18 -0700

    fixed

--- js/project_replace.js
+++ js/project_replace.js
@@ -1,11 +1,13 @@
 const PROJECT1 = "../projects/project1.html";
 const PROJECT2 = "../projects/project2.html";
 const PROJECT3 = "../projects/project3.html";
+const PROJECT4 = "../projects/project4.html";
 
 document.addEventListener("DOMContentLoaded", function () {
   loadProjectSection("project1", PROJECT1, "#keyInfo");
   loadProjectSection("project2", PROJECT2, "#keyInfo");
   loadProjectSection("project3", PROJECT3, "#keyInfo");
+  loadProjectSection("project4", PROJECT4, "#keyInfo");
 });
 
 function loadProjectSection(projectId, file, selector) {
--- js/size-change.js
+++ js/size-change.js
@@ -2,32 +2,47 @@ $(document).ready(function () {
   const overlay1 = $(".portfolioPiece1");
   const overlay2 = $(".portfolioPiece2");
   const overlay3 = $(".portfolioPiece3");
-  const portfolioLink = $(".portfolio a"); // Select the link in the portfolio section
+  const overlay4 = $(".portfolioPiece4");
+  const portfolioLink = $(".portfolio a");
 
-  // Hover event for Project Title 1
+  // Project 1
   $("#project-title-1").hover(function () {
     console.log("hover 1 work");
     overlay2.fadeOut(500);
     overlay3.fadeOut(500);
+    overlay4.fadeOut(500);
     overlay1.fadeIn(500);
-    portfolioLink.attr("href", "30-30.html"); // Change href for Project 1
+    portfolioLink.attr("href", "30-30.html");
   });
 
-  // Hover event for Project Title 2
+  // Project 2
   $("#project-title-2").hover(function () {
     console.log("hover 2 work");
     overlay1.fadeOut(500);
     overlay3.fadeOut(500);
+    overlay4.fadeOut(500);
     overlay2.fadeIn(500);
-    portfolioLink.attr("href", "susan-rodgers.html"); // Change href for Project 2
+    portfolioLink.attr("href", "susan-rodgers.html");
   });
 
-  // Hover event for Project Title 3
+  // Project 3
   $("#project-title-3").hover(function () {
     console.log("hover 3 work");
     overlay1.fadeOut(500);
     overlay2.fadeOut(500);
+    overlay4.fadeOut(500);
     overlay3.fadeIn(500);
-    portfolioLink.attr("href", "lil-wood-diner.html"); // Change href for Project 3
+    portfolioLink.attr("href", "lil-wood-diner.html");
+  });
+
+  // Project 4
+  $("#project-title-4").hover(function () {
+    console.log("hover 4 work");
+    overlay1.fadeOut(500);
+    overlay2.fadeOut(500);
+    overlay3.fadeOut(500);
+    overlay4.fadeIn(500);
+    portfolioLink.attr("href", "last-minute.html");
   });
 });
+
